8b10960f64ce253740341d5af9a91f6038f32efa
eniuane <eniuane@192.168.1.2>
2016-04-09 17:18:51 +0100

    made the function readAllDoubles.

--- EDA_Project/src/pt/europeia/eda/secondDeliver/InsertionTester.java
+++ EDA_Project/src/pt/europeia/eda/secondDeliver/InsertionTester.java
@@ -15,13 +15,14 @@ public class InsertionTester {
 		// final In inShuffled = new In(arguments[1]);
 		// final In inSorted = new In(arguments[2]);
 
-		final String[] partiallySortedNumbers = inPartiallySorted.readAllLines();
-		final Double[] partiallySortedNumbersDouble = new Double[partiallySortedNumbers.length];
+		//final String[] partiallySortedNumbers = inPartiallySorted.readAllStrings();
+		//final Double[] partiallySortedNumbersDouble = new Double[partiallySortedNumbers.length];
+		final Double[] partiallySortedNumbersDouble= readAllDoubles(inPartiallySorted);
 		// final double[] shuffledNumbers = inShuffled.readAllInts();
 		// final double[] sortedNumbers = inSorted.readAllInts();
 
-		for (int i = 0; i != partiallySortedNumbers.length; i++)
-			partiallySortedNumbersDouble[i] = Double.parseDouble(partiallySortedNumbers[i]);
+		//for (int i = 0; i != partiallySortedNumbers.length; i++)
+		//	partiallySortedNumbersDouble[i] = Double.parseDouble(partiallySortedNumbers[i]);
 
 		final Stopwatch stopwatch = new Stopwatch();
 		Insertion.sort(partiallySortedNumbersDouble);
@@ -29,9 +30,18 @@ public class InsertionTester {
 
 		out.println(estimatedTime);
 		
-		//for (int i = 0; i != partiallySortedNumbers.length; i++)
-		//	out.println(partiallySortedNumbersDouble[i]);
+		for (int i = 0; i != partiallySortedNumbersDouble.length; i++)
+			out.println(partiallySortedNumbersDouble[i]);
 
 	}
+	
+	
+	public static Double[] readAllDoubles(In in) {
+        String[] fields = in.readAllStrings();
+        Double[] vals = new Double[fields.length];
+        for (int i = 0; i < fields.length; i++)
+            vals[i] = Double.parseDouble(fields[i]);
+        return vals;
+    }
 
 }
