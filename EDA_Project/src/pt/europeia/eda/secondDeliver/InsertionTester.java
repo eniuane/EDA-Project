8a550341fea9e566942454a075924b4e9bfff678
eniuane <eniuane@192.168.1.2>
2016-04-09 16:54:35 +0100

    Deleted the class SumExecutionTimeTester. Did some tests on the InsertionTester.

--- EDA_Project/src/pt/europeia/eda/secondDeliver/InsertionTester.java
+++ EDA_Project/src/pt/europeia/eda/secondDeliver/InsertionTester.java
@@ -1,24 +1,36 @@
 package pt.europeia.eda.secondDeliver;
 
 import edu.princeton.cs.algs4.In;
+import pt.europeia.eda.Stopwatch;
+
 import static pt.europeia.eda.Tools.out;
 
 public class InsertionTester {
 
 	public static void main(final String[] arguments) {
 
+		double estimatedTime = 0;
+
 		final In inPartiallySorted = new In(arguments[0]);
 		// final In inShuffled = new In(arguments[1]);
 		// final In inSorted = new In(arguments[2]);
-		
-		final Double[] partiallySortedNumbers = inPartiallySorted.readAllDoubles();
+
+		final String[] partiallySortedNumbers = inPartiallySorted.readAllLines();
+		final Double[] partiallySortedNumbersDouble = new Double[partiallySortedNumbers.length];
 		// final double[] shuffledNumbers = inShuffled.readAllInts();
 		// final double[] sortedNumbers = inSorted.readAllInts();
 
 		for (int i = 0; i != partiallySortedNumbers.length; i++)
-			out.println(partiallySortedNumbers[i]);
+			partiallySortedNumbersDouble[i] = Double.parseDouble(partiallySortedNumbers[i]);
 
-		Insertion.sort(partiallySortedNumbers);
+		final Stopwatch stopwatch = new Stopwatch();
+		Insertion.sort(partiallySortedNumbersDouble);
+		estimatedTime = stopwatch.elapsedTime();
+
+		out.println(estimatedTime);
+		
+		//for (int i = 0; i != partiallySortedNumbers.length; i++)
+		//	out.println(partiallySortedNumbersDouble[i]);
 
 	}
 
