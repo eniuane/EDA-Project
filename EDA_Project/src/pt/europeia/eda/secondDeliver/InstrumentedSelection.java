89ee564bad1c650f15e08847847ebf98755e79bb
João Lopes <Joo Lopes@PauloLopes-PC>
2016-04-11 18:33:46 +0100

    Made the InstrumentedSelection.

--- EDA_Project/src/pt/europeia/eda/secondDeliver/InstrumentedSelection.java
+++ EDA_Project/src/pt/europeia/eda/secondDeliver/InstrumentedSelection.java
@@ -1,47 +1,51 @@
 package pt.europeia.eda.secondDeliver;
 
 public class InstrumentedSelection {
-	
+
 	private InstrumentedSelection() {
-        throw new RuntimeException("Attempt to instantiate package-class");
-    }
+		throw new RuntimeException("Attempt to instantiate package-class");
+	}
 
-    public static <Item extends Comparable<? super Item>> void sort(
-            final Item[] values) {
-        for (int numberOfSortedItems = 0;
-             numberOfSortedItems < values.length - 1;
-             numberOfSortedItems++) {
-            int indexOfMinimum = numberOfSortedItems;
+	private static int numberOfComparisons = 0;
+	private static int numberOfArrayReads = 0;
+	private static int numberOfArrayWrites = 0;
+	private static int numberOfSwaps = 0;
 
-            for (int i = indexOfMinimum + 1; i != values.length; i++)
-                if (isLess(values[i], values[indexOfMinimum]))
-                    indexOfMinimum = i;
+	public static <Item extends Comparable<? super Item>> void sort(final Item[] values) {
+		for (int numberOfSortedItems = 0; numberOfSortedItems < values.length - 1; numberOfSortedItems++) {
+			int indexOfMinimum = numberOfSortedItems;
 
-            swap(values, numberOfSortedItems, indexOfMinimum);
-        }
+			for (int i = indexOfMinimum + 1; i != values.length; i++) {
+				numberOfArrayReads += 2;
+				if (isLess(values[i], values[indexOfMinimum]))
+					indexOfMinimum = i;
+			}
+			
+			swap(values, numberOfSortedItems, indexOfMinimum);
+		}
 
-        assert isIncreasing(values) :
-            "Array should be increasing after sorting.";
-    }
+		assert isIncreasing(values) : "Array should be increasing after sorting.";
+	}
 
-    private static <Value extends Comparable<? super Value>> boolean isLess(
-            final Value first, final Value second) {
-        return first.compareTo(second) < 0;
-    }
+	private static <Value extends Comparable<? super Value>> boolean isLess(final Value first, final Value second) {
+		numberOfComparisons++;
+		return first.compareTo(second) < 0;
+	}
 
-    private static void swap(final Object[] values, final int firstPosition,
-            final int secondPosition) {
-        final Object temporary = values[firstPosition];
-        values[firstPosition] = values[secondPosition];
-        values[secondPosition] = temporary;
-    }
+	private static void swap(final Object[] values, final int firstPosition, final int secondPosition) {
+		numberOfArrayWrites += 2;
+		numberOfArrayReads += 2;
+		numberOfSwaps++;
+		final Object temporary = values[firstPosition];
+		values[firstPosition] = values[secondPosition];
+		values[secondPosition] = temporary;
+	}
 
-    private static <Item extends Comparable<? super Item>> boolean isIncreasing(
-            final Item[] values) {
-        for (int i = 1; i < values.length; i++)
-            if (isLess(values[i], values[i - 1]))
-                return false;
-        return true;
-    }
+	private static <Item extends Comparable<? super Item>> boolean isIncreasing(final Item[] values) {
+		for (int i = 1; i < values.length; i++)
+			if (isLess(values[i], values[i - 1]))
+				return false;
+		return true;
+	}
 
 }
