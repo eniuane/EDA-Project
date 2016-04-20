7db4691d08ee9ece9b73c3357bc9007631a98c16
João Lopes <Joo Lopes@PauloLopes-PC>
2016-04-20 16:21:22 +0100

    Optimized the code, no more need of those files.

--- EDA_Project/src/pt/europeia/eda/thirdDeliver/QuickTester.java
+++ EDA_Project/src/pt/europeia/eda/thirdDeliver/QuickTester.java
@@ -7,10 +7,12 @@ import java.util.Arrays;
 
 import edu.princeton.cs.algs4.In;
 import pt.europeia.eda.Stopwatch;
-import pt.europeia.eda.thirdDeliver.Quick;
 
 public class QuickTester {
-	public static final double timeBudgetPerExperiment = 30.0 /* seconds */;
+
+	public static final double timeBudgetPerExperiment = 2.0 /* seconds */;
+
+	public static final double maxTimeForAnExperiment = 30.0;
 
 	public static final double minimumTimePerContiguousRepetitions = 1e-5 /* seconds */;
 
@@ -48,6 +50,7 @@ public class QuickTester {
 			System.arraycopy(originalArray, 0, arrayToSort, 0, originalArray.length);
 			Quick.sort(arrayToSort);
 			contiguousRepetitions++;
+
 		} while (stopwatch.elapsedTime() < minimumTimePerContiguousRepetitions);
 
 		return contiguousRepetitions;
@@ -58,7 +61,7 @@ public class QuickTester {
 		final Double[] numbersToSortSorted = readAllDoubles(in);
 		final ArrayList<Double[]> listOfNumbersToSort = new ArrayList<Double[]>();
 		for (int i = 0; i != contiguousRepetitions; i++)
-			listOfNumbersToSort.add(Arrays.copyOf(numbersToSortSorted, numbersToSortSorted.length));
+			listOfNumbersToSort.add(numbersToSortSorted);
 
 		final Stopwatch stopwatch = new Stopwatch();
 		for (int i = 0; i != contiguousRepetitions; i++) {
@@ -82,13 +85,11 @@ public class QuickTester {
 		final double median = medianOf(executionTimes);
 		final double average = averageOf(executionTimes);
 
-		if (!isWarmup)
-			out.println("Sorted file " + "data/partially_sorted_" + limit + ".txt" + " \t median= " + median
-					+ "\t Average= " + average + "\t Minimum= " + executionTimes.get(0) + "\t Maximum= "
-					+ executionTimes.get(executionTimes.size() - 1) + "\t Reps= " + repetitions);
-
-		if (repetitions == 1)
-			System.exit(1);
+		if (!isWarmup) {
+			out.println("Sorted " + limit + " \t median= " + median + "\t Average= " + average + "\t Minimum= "
+					+ executionTimes.get(0) + "\t Maximum= " + executionTimes.get(executionTimes.size() - 1)
+					+ "\t Reps= " + repetitions);
+		}
 	}
 
 	public static void main(final String[] arguments) throws InterruptedException {
@@ -96,9 +97,13 @@ public class QuickTester {
 		for (int exponent = 0, limit = 2; exponent != 8; exponent++, limit *= 2)
 			performExperimentsFor(limit, true);
 
-		for (int exponent = 0, limit = 2; exponent != 24; exponent++, limit *= 2)
+		for (int exponent = 0, limit = 2; exponent != 24; exponent++, limit *= 2) {
+			final Stopwatch stopwatch = new Stopwatch();
 			performExperimentsFor(limit, false);
+			if (stopwatch.elapsedTime() > maxTimeForAnExperiment)
+				break;
 
+		}
 	}
 
 	public static Double[] readAllDoubles(In in) {
