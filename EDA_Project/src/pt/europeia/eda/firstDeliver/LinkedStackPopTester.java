6749e3d6cceb15850ebb6ea1ecbb0a63aa7f9ef4
João Lopes <Joo Lopes@PauloLopes-PC>
2016-04-22 17:17:56 +0100

    deleted the classes for tests.

--- EDA_Project/src/pt/europeia/eda/firstDeliver/LinkedStackPopTester.java
+++ EDA_Project/src/pt/europeia/eda/firstDeliver/LinkedStackPopTester.java
@@ -1,58 +1,136 @@
 package pt.europeia.eda.firstDeliver;
 
-import static pt.europeia.eda.Tools.out;
+import static java.lang.System.out;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.security.AllPermission;
 
 import pt.europeia.eda.Stopwatch;
 
+import java.util.ArrayList;
+
 public class LinkedStackPopTester {
 
-	public static void main(String[] args) {
+	public static final double timeBudgetPerExperiment = 2.0 /* seconds */;
 
-		int maxReps = 100;
-		double estimatedTime = 0;
+	public static final double maxTimeForAnExperiment = 30.0;
 
-		for (int limit = 1, exponent = 0; exponent != 31; exponent++, limit *= 2) {
+	public static final double minimumTimePerContiguousRepetitions = 1e-5 /* seconds */;
 
-			double[] timers = new double[maxReps];
+	public static double medianOf(final ArrayList<Double> values) {
+		final int size = values.size();
 
-			double sumOfTimers = 0;
+		values.sort(null);
 
-			for (int i = 0; i < maxReps; i++) {
-				final LinkedStack<Integer> stackOfInts = new LinkedStack<Integer>();
+		if (size % 2 == 0)
+			return (values.get(size / 2 - 1) + values.get(size / 2)) / 2.0;
+		else
+			return values.get(size / 2);
+	}
 
-				for (int j = 0; j < limit; j++)
-					stackOfInts.push(i);
+	public static double averageOf(final ArrayList<Double> values) {
+		double sum = 0.0;
 
-				final Stopwatch stopwatch = new Stopwatch();
+		if (values.size() == 1)
+			return values.get(0);
 
-				for (int j = 0; j < limit; j++)
-					stackOfInts.pop();
+		for (int i = 0; i != values.size() - 1; i++)
+			sum += values.get(i);
 
-				estimatedTime = stopwatch.elapsedTime();
-				timers[i] = estimatedTime;
-				sumOfTimers = sumOfTimers + timers[i];
+		return sum / values.size();
+	}
 
+	public static int contiguousRepetitionsFor(final int limit) {
+		int contiguousRepetitions = 1;
+		for (int exponent = 0; exponent != 31; exponent++, contiguousRepetitions *= 2) {
+			final ArrayList<LinkedStack<Integer>> linkedStacks = new ArrayList<LinkedStack<Integer>>();
+			for (int i = 0; i != contiguousRepetitions; i++) {
+				linkedStacks.add(new LinkedStack<Integer>());
+				for (int j = 0; j != limit; j++)
+					linkedStacks.get(i).push(j);
 			}
 
-			Arrays.sort(timers);
-			int middle = timers.length / 2;
-			double median = 0;
+			final Stopwatch stopwatch = new Stopwatch();
+			for (int i = 0; i != contiguousRepetitions; i++) {
+				final LinkedStack<Integer> linkedStack = linkedStacks.get(i);
+				for (int j = 0; j != limit; j++) {
+					linkedStack.pop();
+				}
+				linkedStacks.set(i, null);
+			}
+			if (stopwatch.elapsedTime() >= minimumTimePerContiguousRepetitions)
+				break;
+		}
 
-			if (timers.length % 2 == 0)
-				median = (timers[middle] + timers[middle - 1]) / 2;
-			else
-				median = timers[middle];
+		return contiguousRepetitions;
+	}
 
-			double averageOfTimers = 0;
+	public static double executionTimeFor(final int limit, final int contiguousRepetitions) {
 
-			averageOfTimers = sumOfTimers / maxReps;
+		final ArrayList<LinkedStack<Integer>> linkedStacks = new ArrayList<LinkedStack<Integer>>();
+		for (int i = 0; i != contiguousRepetitions; i++) {
+			linkedStacks.add(new LinkedStack<Integer>());
+			for (int j = 0; j != limit; j++)
+				linkedStacks.get(i).push(j);
+		}
 
-			out.println("Pop " + limit + " times, Mediana = " + median + " Max = " + timers[timers.length - 1]
-					+ " Min = " + timers[0] + " Sum = " + sumOfTimers + " Average = " + averageOfTimers);
+		final Stopwatch stopwatch = new Stopwatch();
+		for (int i = 0; i != contiguousRepetitions; i++) {
+			final LinkedStack<Integer> linkedStack = linkedStacks.get(i);
+			for (int j = 0; j != limit; j++) {
+				linkedStack.pop();
+			}
+			linkedStacks.set(i, null);
+		}
+		return stopwatch.elapsedTime() / contiguousRepetitions;
+	}
 
+	public static void performExperimentsFor(final int limit, final boolean isWarmup) {
+		final ArrayList<Double> executionTimes = new ArrayList<Double>();
+		final Stopwatch stopwatch = new Stopwatch();
+		final int contiguousRepetitions = contiguousRepetitionsFor(limit);
+		long repetitions = 0;
+		do {
+			executionTimes.add(executionTimeFor(limit, contiguousRepetitions));
+			repetitions++;
+		} while (stopwatch.elapsedTime() < timeBudgetPerExperiment);
+
+		final double median = medianOf(executionTimes);
+		final double average = averageOf(executionTimes);
+
+		if (!isWarmup)
+			out.println("Made " + limit + " pops \t median= " + median + "\t Average= " + average + "\t Minimum= "
+					+ executionTimes.get(0) + "\t Maximum= " + executionTimes.get(executionTimes.size() - 1)
+					+ "\t Reps= " + repetitions + "\t ContiguousReps= " + contiguousRepetitions);
+	}
+
+	public static void main(final String[] arguments) throws InterruptedException {
+
+		for (int exponent = 0, limit = 1; exponent != 8; exponent++, limit *= 2)
+			performExperimentsFor(limit, true);
+
+		for (int exponent = 0, limit = 1; exponent != 31; exponent++, limit *= 2) {
+			final Stopwatch stopwatch = new Stopwatch();
+			performExperimentsFor(limit, false);
+			if (stopwatch.elapsedTime() > maxTimeForAnExperiment)
+				break;
 		}
 	}
 
 }
+
+/*
+ * Copyright 2016, Manuel Menezes de Sequeira.
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ */
